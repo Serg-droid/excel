879b8703d116fe6e4a556f8519b66e050d3ab870
Сергей Худяков <sergei.hudakov@gmail.com>
2020-07-06 22:10:25 +0300

    finished table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,11 @@
 export class Dom {
+
   constructor(selector) {
   this.$el = typeof selector === 'string'
     ? document.querySelector(selector)
     : selector;
   }
+
   html(html) {
     if(typeof html === 'string'){
       this.$el.innerHTML = html;
@@ -11,16 +13,28 @@ export class Dom {
     }
     return this.$el.outerHTML.trim();
   }
+
   clear() {
     this.html('');
     return this;
   }
+
   on(eventType, callback = f=>f) {
     this.$el.addEventListener(eventType, callback);
   }
+
   off(eventType, callback = f=>f) {
     this.$el.removeEventListener(eventType, callback);
   }
+
+  get data() {
+    return this.$el.dataset;
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
   append(node) {
     if(node instanceof Dom){
       node = node.$el;
@@ -31,6 +45,19 @@ export class Dom {
       this.$el.appendChild(node);
     }
     return this;
+  }
+
+  css(styles = {}){
+    Object.assign(this.$el.style, styles);
+    return this;
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector));
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect();
   }
 }
 
